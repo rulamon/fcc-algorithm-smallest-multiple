015dde7291a54792e68b0384508d87fd3a7d2407
rulamon <ruben.lamon@gmail.com>
2018-12-01 14:31:21 +0100

    edited first solution, using sort

--- 1.js
+++ 1.js
@@ -1,12 +1,11 @@
 //function to create array of sequential numbers between and including parameters
 const seqNum = (arr) => {
 	//store lowest and highest parameters in variable and create a new, empty array to push sequential between parameters numbers into
-	let lowest = Math.min(...arr)
-	let highest = Math.max(...arr)
-	let seqArr = []
+	arr.sort((a, b) => a + b);
+	let seqArr = [];
 	//push all sequential numbers between and including the parameters into new array
-	for (let num = lowest; num <= highest; num++) {
-		seqArr.push(num)
+	for (let num = arr[1]; num <= arr[0]; num++) {
+		seqArr.unshift(num);
 	}
 	return seqArr;
 }
@@ -14,14 +13,14 @@ const seqNum = (arr) => {
 function smallestCommons(arr) {
 	//call seqArr to get array of sequential numbers
 	let seqArr = seqNum(arr);
-	//create basis = product of last two numbers in array
-	let basis = seqArr[seqArr.length - 1] * seqArr[seqArr.length - 2];
-	let result = basis;
-	//add basis to result until modulus with all numbers in array equals 0
+	//create start = product of last two numbers in array
+	let start = seqArr[0] * seqArr[1];
+	let result = start;
+	//add start to result until modulus with all numbers in array equals 0
 	while (seqArr.some(el => result % el != 0)) {
-		result += basis;
+		result += start;
 	}
-	return result
+	return result;
 }
 
 console.log(smallestCommons([5, 10]))
